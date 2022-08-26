b34b7a3b6f8de4b8d537c8dca8cd0d59c46bdb55
NursSSS <nurik1kun@gmail.com>
2022-08-26 19:24:04 +0600

    test 2

--- src/configs/orm.config.ts
+++ src/configs/orm.config.ts
@@ -11,5 +11,8 @@ export const config: TypeOrmModuleOptions = {
     password: process.env.DB_PASSWORD+'',
     database: process.env.DB_NAME,
     entities: ['dist/**/*.entity{.ts,.js}'],
-    synchronize: true
+    synchronize: true,
+    ssl: {
+        rejectUnauthorized: false,
+    }
 }
